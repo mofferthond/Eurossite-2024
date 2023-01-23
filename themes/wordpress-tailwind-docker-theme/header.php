16f5fcb53862903e3fcac3b27c62c311b6da1462
Benjamin Bourgeois <benjishift@gmail.com>
2023-01-23 17:36:34 +0100

    :rocket: update viewport in header.php

--- themes/wordpress-tailwind-docker-theme/header.php
+++ themes/wordpress-tailwind-docker-theme/header.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="initial-scale=1.0, width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
