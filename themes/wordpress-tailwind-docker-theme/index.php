234383daeaef10d39f9ecbeee89a47e081a7e74b
Daniel Kuiper <daniel.kuiper@live.nl>
2024-03-27 20:19:19 +0100

    add singe page content

--- themes/wordpress-tailwind-docker-theme/index.php
+++ themes/wordpress-tailwind-docker-theme/index.php
@@ -1,6 +1,22 @@
 <?php get_header(); ?>
 
-<main></main>
+<main>
+    <div class="container mx-auto my-8">
+        <?php if (have_posts()) : ?>
+            <?php
+            while (have_posts()) :
+                the_post();
+            ?>
+                <article>
+                    <h1 class="text-4xl font-bold"><?php the_title(); ?></h1>
+                    <div class="my-4">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+</main>
 
 <?php
 get_footer();
